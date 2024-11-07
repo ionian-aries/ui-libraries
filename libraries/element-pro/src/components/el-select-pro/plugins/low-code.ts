0b56b9613ea34bb953e067be7e4888710c61e2d2
刘宇阳 <myronliu347@gmail.com>
2024-11-07 09:49:04 +0800

    chore(ElSelectPro): 选择器ide 环境适配

--- libraries/element-pro/src/components/el-select-pro/plugins/low-code.ts
+++ libraries/element-pro/src/components/el-select-pro/plugins/low-code.ts
@@ -2,7 +2,7 @@ import { NaslComponentPluginOptions } from '@lcap/vue2-utils';
 
 /* 仅在 ide 环境生效的插件 */
 export const useLowcodeStyle: NaslComponentPluginOptions = {
-  setup(props) {
+  setup(props, { setupContext: ctx }) {
     const value = props.useComputed(['dataSource', 'multiple'], (d, multiple) => {
       if (!Array.isArray(d) || !d.length) {
         return null;
@@ -12,6 +12,14 @@ export const useLowcodeStyle: NaslComponentPluginOptions = {
     });
     return {
       value,
+      popupProps: props.useComputed('popupProps', (popupProps = {}) => {
+        return {
+          ...popupProps,
+          attach: () => {
+            return ctx.refs.$base?.$el;
+          },
+        };
+      }),
     };
   },
   onlyUseIDE: true,
